bdd0af5fd61ee192d73258ea6303377ff3e68411
Leonardo Dominguez Martinez <113126666+Sorlux@users.noreply.github.com>
2024-11-24 00:05:09 -0700

    Update Server_Test.cpp

--- Microcontrollers/ESP32/Tests/Server_Test.cpp
+++ Microcontrollers/ESP32/Tests/Server_Test.cpp
@@ -1,11 +1,18 @@
 #include <WiFi.h>
 
 // WiFi credentials
-#define WIFI_SSID "Placeholder"
-#define WIFI_PASSWORD "Placeholder"
+#define WIFI_SSID "Galaxy S20+"
+#define WIFI_PASSWORD "135790864"
+
+/*
+  Will need two leds and resistors where one is using the 16 GPIO and the second is using the 17 GPIO
+*/
 
 WiFiServer server(80); // Create a web server on port 80
-
+String firstLEDState = "off";
+String secondLEDState = "off";
+unsigned int firstPIN = 16;
+unsigned int secondPIN = 17;
 
 
 void setup() {
@@ -13,6 +20,13 @@ void setup() {
   Serial.begin(921600);
   pinMode(LED_BUILTIN, OUTPUT); // Set LED pin as output
   digitalWrite(LED_BUILTIN, LOW); // Turn off the LED initially
+
+
+  pinMode(firstPIN, OUTPUT);
+  digitalWrite(firstPIN, LOW);
+
+  pinMode(secondPIN, OUTPUT);
+  digitalWrite(secondPIN, LOW);
 
   // Connect to WiFi
   Serial.print("Connecting to WiFi");
@@ -44,25 +58,49 @@ void loop() {
   Serial.println("Request: " + request);
 
   // Handle the HTTP request
-  if (request.indexOf("/LED=ON") != -1) {
-    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on
+  if (request.indexOf("/16PINLED=ON") != -1) {
+    digitalWrite(firstPIN, HIGH); // Turn the LED on
     Serial.println("LED turned ON");
-  } else if (request.indexOf("/LED=OFF") != -1) {
-    digitalWrite(LED_BUILTIN, LOW); // Turn the LED off
+    firstLEDState = "on";
+  } else if (request.indexOf("/16PINLED=OFF") != -1) {
+    digitalWrite(firstPIN, LOW); // Turn the LED off
     Serial.println("LED turned OFF");
+    firstLEDState = "off";
+  } else if (request.indexOf("/17PINLED=ON") != -1) {
+    digitalWrite(secondPIN, HIGH);
+    Serial.println("LED turned on");
+    secondLEDState = "on";
+  } else if (request.indexOf("17PINLED=OFF") != -1) {
+    digitalWrite(secondPIN, LOW);
+    Serial.println("LED turned off");
+    secondLEDState = "off";
   }
 
-  // Send a simple HTML response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println("Connection: close");
-  client.println();
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
-  client.println("<h1>ESP32 Web Server</h1>");
-  client.println("<p><a href='/LED=ON'>Turn LED ON</a></p>");
-  client.println("<p><a href='/LED=OFF'>Turn LED OFF</a></p>");
-  client.println("</html>");
-  client.stop(); // Close the connection
-  Serial.println("Client disconnected.");
+// Display the HTML web page
+client.println("<!DOCTYPE html><html>");
+client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+client.println("<link rel=\"icon\" href=\"data:,\">");
+// CSS to style the on/off buttons
+client.println("<style>html { font-family: monospace; display: inline-block; margin: 0px auto; text-align: center;}");
+client.println(".button { background-color: yellowgreen; border: none; color: white; padding: 16px 40px;");
+client.println("text-decoration: none; font-size: 32px; margin: 2px; cursor: pointer;}");
+client.println(".button2 {background-color: gray;}</style></head>");
+
+client.println("<body><h1>ESP32 Web Server</h1>");
+client.println("<p>Control LED State</p>");
+
+if (firstLEDState == "off") {
+  client.println("<p><a href=\"/16PINLED=ON\"><button class=\"button\">ON</button></a></p>");
+} else {
+  client.println("<p><a href=\"/16PINLED=OFF\"><button class=\"button button2\">OFF</button></a></p>");
+}
+if (secondLEDState == "off") {
+  client.println("<p><a href=\"/17PINLED=ON\"><button class=\"button\">ON</button></a></p>");
+} else {
+  client.println("<p><a href=\"/17PINLED=OFF\"><button class=\"button button2\">OFF</button></a></p>");
+}
+client.println("</body></html>");
+
+// The HTTP response ends with another blank line
+client.println();
 }
